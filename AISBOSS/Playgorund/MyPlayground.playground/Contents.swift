7cf74624b2101cae40ded4a4dd2e10c5ee2b9740
nicolastinkl <nicolastinkl@gmail.com>
2015-09-09 15:16:56 +0800

    #Cache

---
+++ AISBOSS/Playgorund/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+extension String {
+    var banana : String {
+//        let shortName = String(self.characters.dropFirst(1))
+//        return "\(self) \(self) Bo B\(shortName) Banana Fana Fo F\(shortName)"
+        
+        return " \(self.characters.first!)"
+    }
+}
+
+let bananaName = "Jimmy".banana		// "Jimmy Jimmy Bo Bimmy Banana Fana Fo Fimmy"
