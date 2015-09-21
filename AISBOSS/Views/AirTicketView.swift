96dac2620e6846a1b99fb1a17428b2ca61dd5757
liliang <liliang9@asiainfo.com>
2015-09-21 11:33:23 +0800

    添加机票视图

---
+++ AISBOSS/Views/AirTicketView.swift
@@ -0,0 +1,28 @@
+//
+//  AirTicketView.swift
+//  AIVeris
+//
+//  Created by admin on 15/9/21.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class AirTicketView: UIView {
+
+    @IBOutlet weak var flightNumber: UILabel!
+    @IBOutlet weak var startAirport: UILabel!
+    @IBOutlet weak var finishAirport: UILabel!
+    @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var arriveDate: UILabel!
+    @IBOutlet weak var ticketType: UILabel!
+    @IBOutlet weak var priceAndPassengerType: UILabel!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
