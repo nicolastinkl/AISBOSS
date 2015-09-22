e23fb70895e6a60081ed8f69db69be8cf544bddd
liliang <liliang9@asiainfo.com>
2015-09-22 14:01:25 +0800

    修改了“跳动数字”控件

--- AISBOSS/Views/JumpNumberLabel.swift
+++ AISBOSS/Views/JumpNumberLabel.swift
@@ -23,28 +23,44 @@ class JumpNumberLabel: UILabel {
     private var timer: NSTimer?
     private var changeTimeInterval: NSTimeInterval!
     private var isFloat: Bool!
+    private var decimalFormat: NSNumberFormatter?
     
     
     var formatFloatStr = DEFAULT_FLOAT_FORMAT
     var formatIntStr = DEFAULT_INT_FORMAT
-
+    
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
+    // Drawing code
     }
     */
     
-    func changeFloatNumberTo(toNumber: Float, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_FLOAT_FORMAT) {
-        changeFloatNumberFrom(currentNumber, toNumber: toNumber, duration: duration, format: format)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
     }
     
-    func changeFloatNumberFrom(fromNumber: Float, toNumber: Float, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_FLOAT_FORMAT) {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    private func prepareView() {
+        
+    }
+    
+    func changeFloatNumberTo(toNumber: Float, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_FLOAT_FORMAT, numberFormat: NSNumberFormatter? = nil) {
+        changeFloatNumberFrom(currentNumber, toNumber: toNumber, duration: duration, format: format, numberFormat: numberFormat)
+    }
+    
+    func changeFloatNumberFrom(fromNumber: Float, toNumber: Float, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_FLOAT_FORMAT, numberFormat: NSNumberFormatter? = nil) {
         currentNumber = fromNumber
         destinationFloat = toNumber
         changeTimeInterval = duration / Double(defaultJumpCount)
         formatFloatStr = format
+        decimalFormat = numberFormat
         isFloat = true
         
         stepFloat = calculateStepNumber(destinationFloat - currentNumber, jumpCount: defaultJumpCount)
@@ -52,21 +68,38 @@ class JumpNumberLabel: UILabel {
         startChangeFloat(duration)
     }
     
-    func changeIntNumberTo(toNumber: Int, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_INT_FORMAT) {
-        changeIntNumberFrom(Int(currentNumber), toNumber: toNumber, duration: duration, format: format)
+    func changeIntNumberTo(toNumber: Int, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_INT_FORMAT, numberFormat: NSNumberFormatter? = nil) {
+        changeIntNumberFrom(Int(currentNumber), toNumber: toNumber, duration: duration, format: format, numberFormat: numberFormat)
     }
     
-    func changeIntNumberFrom(fromNumber: Int, toNumber: Int, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_INT_FORMAT) {
+    func changeIntNumberFrom(fromNumber: Int, toNumber: Int, duration: NSTimeInterval = DEFAULT_DURATION, format: String = DEFAULT_INT_FORMAT, numberFormat: NSNumberFormatter? = nil) {
         currentNumber = Float(fromNumber)
         destinationFloat = Float(toNumber)
         let realJumpCount = min(abs(toNumber - fromNumber), defaultJumpCount)
         changeTimeInterval = duration / Double(realJumpCount)
         formatIntStr = format
+        decimalFormat = numberFormat
         isFloat = false
         
         stepFloat = calculateStepNumber(destinationFloat - currentNumber, jumpCount: realJumpCount)
         
         startChangeFloat(duration)
+    }
+    
+    static func createDefaultFloatCurrencyFormatter() -> NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.formatterBehavior = NSNumberFormatterBehavior.Behavior10_4
+        formatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        formatter.positiveFormat = "###,##0.00"
+        return formatter
+    }
+    
+    static func createDefaultIntCurrencyFormatter() -> NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.formatterBehavior = NSNumberFormatterBehavior.Behavior10_4
+        formatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        formatter.positiveFormat = "###,##0"
+        return formatter
     }
     
     private func calculateStepNumber(numberRange: Float, jumpCount: Int) -> Float {
@@ -107,8 +140,43 @@ class JumpNumberLabel: UILabel {
     }
     
     private func setTextResult() {
-        let outputStr = (isFloat == true) ? NSString(format: formatFloatStr, currentNumber) : NSString(format: formatIntStr, Int(currentNumber + 0.5))
-        text = outputStr as String
+        
+        var outStr: NSString?
+        
+        if isFloat == true {
+            outStr = convertToFloatOutputString()
+        } else {
+            outStr = convertToIntOutputString()
+        }
+        
+        text = outStr as? String
+        
     }
-
+    
+    private func convertToFloatOutputString() -> NSString? {
+        var outStr: NSString?
+        if let numberFormatter = decimalFormat {
+            if let str = numberFormatter.stringFromNumber(NSNumber(float: currentNumber)) {
+                outStr = NSString(format: formatFloatStr, str)
+            }
+        } else {
+            outStr = NSString(format: formatFloatStr, currentNumber)
+        }
+        
+        return outStr
+    }
+    
+    private func convertToIntOutputString() -> NSString? {
+        var outStr: NSString?
+        if let numberFormatter = decimalFormat {
+            if let str = numberFormatter.stringFromNumber(NSNumber(long: Int(currentNumber + 0.5))) {
+                outStr = NSString(format: formatIntStr, str)
+            }
+        } else {
+            outStr = NSString(format: formatIntStr, Int(currentNumber + 0.5))
+        }
+        
+        return outStr
+    }
+    
 }
