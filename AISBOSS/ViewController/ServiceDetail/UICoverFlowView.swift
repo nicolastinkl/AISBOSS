89168b0e0bbaadb14ff23dfbae72976b65921aa2
nicolastinkl <nicolastinkl@gmail.com>
2015-09-30 14:26:13 +0800

    #update

--- AISBOSS/ViewController/ServiceDetail/UICoverFlowView.swift
+++ AISBOSS/ViewController/ServiceDetail/UICoverFlowView.swift
@@ -39,8 +39,15 @@ class UICoverFlowView: UIView {
         lableContent.text = model.service_intro as String
         labelPrice.text = model.service_price.price_show as String
         imageview.setURL(model.service_intro_img?.toURL()!, placeholderImage: UIImage(named: "Placehold"))
-        avatorImageview.setURL(model.service_provider.provider_portrait_icon?.toURL()!, placeholderImage: UIImage(named: "Placehold"))
-        labelNick.text = model.service_provider.provider_name as String
+
+        if model.service_provider.provider_portrait_icon != nil {
+            avatorImageview.setURL(model.service_provider.provider_portrait_icon?.toURL()!, placeholderImage: UIImage(named: "Placehold"))        
+        }
+        
+        if model.service_provider.provider_name != nil {
+            labelNick.text = model.service_provider.provider_name as String
+        }
+        
     }
     
 }
