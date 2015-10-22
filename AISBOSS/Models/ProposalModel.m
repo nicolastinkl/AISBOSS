18792235e76253431147ccf80a4a6a921ed9ddfd
liliang <liliang9@asiainfo.com>
2015-10-22 19:22:58 +0800

    添加了proposal模型

---
+++ AISBOSS/Models/ProposalModel.m
@@ -0,0 +1,28 @@
+//
+//  ProposalModel.m
+//  AIVeris
+//
+//  Created by Rocky on 15/10/22.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+#import "ProposalModel.h"
+
+@implementation ParamModel : JSONModel
+
+@end
+
+
+@implementation ServiceOrderModel
+
+@end
+
+
+@implementation ProposalListModel
+
+@end
+
+
+@implementation ProposalModel
+
+@end
