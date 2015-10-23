7bd05afd848e86b01af63c67163dda6c0d78a0ec
liuxian <liuxian@google.com>
2015-10-23 20:58:35 +0800

    #update 新增海淘商品列表

--- AISBOSS/ViewController/AIOrderCellEShopSubView.swift
+++ AISBOSS/ViewController/AIOrderCellEShopSubView.swift
@@ -10,12 +10,64 @@ import UIKit
 
 class AIOrderCellEShopSubView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var offerIconImageView: UIImageView!
+    @IBOutlet weak var offerNameLabel: UILabel!    
+    @IBOutlet weak var offerPriceLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        //config statusLabel
+        statusLabel.layer.borderWidth = 1
+        statusLabel.layer.borderColor = UIColor.grayColor().CGColor
+        statusLabel.layer.cornerRadius = 3
+        statusLabel.layer.masksToBounds = true
+    }
+    
+    // MARK: currentView
+    class func currentView()->AIOrderCellEShopSubView{
+        let selfView = NSBundle.mainBundle().loadNibNamed("AIOrderCellEShopSubView", owner: self, options: nil).first  as! AIOrderCellEShopSubView
+        return selfView
+    }
+
+
+}
+
+class AIOrderCellEShopView : UIView {
+    
+    let SUB_VIEW_HEIGHT : CGFloat = 104
+    
+    let dataSource = ["0","1"]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func buildSubViews(){
+        let subView1 = AIOrderCellEShopSubView.currentView()
+        let frame1 = CGRectMake(0, 0, self.frame.width, SUB_VIEW_HEIGHT)
+        subView1.frame = frame1
+        
+        let subView2 = AIOrderCellEShopSubView.currentView()
+        let frame2 = CGRectMake(0, SUB_VIEW_HEIGHT, self.frame.width, SUB_VIEW_HEIGHT)
+        subView1.frame = frame2
+        
+        self.addSubview(subView1)
+        self.addSubview(subView2)
+        
+        adjustViewFrame()
+    }
+    
+    func adjustViewFrame(){
+        self.frame.size.height = dataSource.count * SUB_VIEW_HEIGHT
+    }
 }
