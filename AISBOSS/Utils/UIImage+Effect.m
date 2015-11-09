70445ad77f913a611fd2c9c1da3cd4fe865e60f0
tinkl <nicolastinkl@gmail.com>
2015-11-09 11:34:47 +0800

    # Add ImagewithColor and 6c929f

--- AISBOSS/Utils/UIImage+Effect.m
+++ AISBOSS/Utils/UIImage+Effect.m
@@ -232,4 +232,21 @@
     return returnImage;
 }
 
+//改变图片颜色
+- (UIImage *)imageWithColor:(UIColor *)color
+{
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, self.scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, 0, self.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextClipToMask(context, rect, self.CGImage);
+    [color setFill];
+    CGContextFillRect(context, rect);
+    UIImage*newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
 @end
