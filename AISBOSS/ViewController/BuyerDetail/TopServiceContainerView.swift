e4117b0f5c50e6fff430f23019030838a9c8256d
liliang <liliang9@asiainfo.com>
2015-11-09 16:10:40 +0800

    实现了主服务卡片布局的切换

--- AISBOSS/ViewController/BuyerDetail/TopServiceContainerView.swift
+++ AISBOSS/ViewController/BuyerDetail/TopServiceContainerView.swift
@@ -9,5 +9,27 @@
 import UIKit
 
 class TopServiceContainerView: ServiceContainerView {
-
+     
+    var isSingle: Bool = false {
+        willSet(newVaklue) {
+            bottomBall.hidden = newVaklue
+        }
+    }
+    
+    override func isPrimeService(isPrime: Bool) {
+        super.isPrimeService(isPrime)
+        if isPrime {
+            topBall.image = UIImage(named: "white_ball")
+            background.image = UIImage(named: "white_top_unfilled_corner")
+            topBallHeightConstraint.constant = 20
+            
+            
+        } else {
+            topBall.image = UIImage(named: "hollow_ball")
+            background.image = UIImage(named: "white_corner_bk")  
+            topBallHeightConstraint.constant = 16
+        }
+        
+        topBall.setNeedsUpdateConstraints()
+    }
 }
