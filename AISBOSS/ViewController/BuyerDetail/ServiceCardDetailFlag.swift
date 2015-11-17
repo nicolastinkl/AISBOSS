009c024d34f9cbfeb8f245518fd55d6509c280eb
liuxian <liuxian@google.com>
2015-11-17 18:25:50 +0800

    #update 增加服务详情页面

---
+++ AISBOSS/ViewController/BuyerDetail/ServiceCardDetailFlag.swift
@@ -0,0 +1,21 @@
+//
+//  ServiceCardDetailFlag.swift
+//  AIVeris
+//
+//  Created by 刘先 on 15/11/17.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class ServiceCardDetailFlag: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
