545d21ee83a5682bd1008248c9ec46fc2da66cb0
liuxian <liuxian@google.com>
2015-11-20 16:45:41 +0800

    #update 鼓舞卡片

--- AISBOSS/ViewController/BuyerDetail/ServiceCardDetailIcon.swift
+++ AISBOSS/ViewController/BuyerDetail/ServiceCardDetailIcon.swift
@@ -25,14 +25,16 @@ class ServiceCardDetailIcon: UIView {
 
     //MARK: - Constants
     //sizes
-    let VIEW_LEFT_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(98)
-    let VIEW_TOP_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(56)
-    let ICON_TITLE_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(40)
-    let ICONS_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(194)
-    let ICON_LABEL_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(34)
+    let ICON_SIZE : CGFloat = AITools.displaySizeFrom1080DesignSize(62)
+    let VIEW_HEIGHT : CGFloat = AITools.displaySizeFrom1080DesignSize(300)
+    let VIEW_LEFT_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(40)
+    let VIEW_TOP_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(10)
+    let ICON_TITLE_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(68)
+    let ICONS_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(200)
+    let ICON_LABEL_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(64)
     //fonts
-    let TITLE_TEXT_FONT : UIFont = AITools.myriadRegularWithSize(17)
-    let ICON_DESC_FONT : UIFont = AITools.myriadRegularWithSize(17)
+    let TITLE_TEXT_FONT : UIFont = AITools.myriadSemiCondensedWithSize(56/2.5)
+    let ICON_DESC_FONT : UIFont = AITools.myriadLightSemiCondensedWithSize(48/2.5)
     
     //MARK: - init
     override init(frame: CGRect) {
@@ -46,12 +48,22 @@ class ServiceCardDetailIcon: UIView {
     //MARK: - load data
     func loadData(){
         layoutView()
+        
+        timeIconImageView.image = UIImage(named: "icon_time_big")
+        priceIconImageView.image = UIImage(named: "icon_price_big")
+        calendaIconImageView.image = UIImage(named: "icon_calenda_big")
+        
+        timeLabelView.text = "64 hours"
+        priceLabelView.text = "$99/hours"
+        calendaLabelView.text = "every 4 weeks"
     }
     
     func layoutView(){
+        //self.backgroundColor = UIColor.redColor()
         buildTitle()
         buildIcon()
         buildIconDesc()
+        fixFrame()
     }
     
     //MARK: - build views
@@ -65,10 +77,10 @@ class ServiceCardDetailIcon: UIView {
         self.addSubview(titleLabel)
         
         layout(titleLabel){titleLabel in
-            titleLabel.leadingMargin == titleLabel.superview!.leadingMargin + VIEW_LEFT_MARGIN
-            titleLabel.trailingMargin == titleLabel.superview!.trailingMargin
-            titleLabel.topMargin == titleLabel.superview!.topMargin + VIEW_TOP_MARGIN
-            titleLabel.height == 21
+            titleLabel.leadingMargin == titleLabel.superview!.leadingMargin
+            titleLabel.trailingMargin >= titleLabel.superview!.trailingMargin
+            titleLabel.topMargin == titleLabel.superview!.topMargin
+            titleLabel.height == 20
         }
     }
     
@@ -77,27 +89,88 @@ class ServiceCardDetailIcon: UIView {
         priceIconImageView = UIImageView(frame : CGRectZero)
         calendaIconImageView = UIImageView(frame : CGRectZero)
         
-        layout(timeIconImageView,priceIconImageView,calendaIconImageView){
-            timeIconImageView,priceIconImageView,calendaIconImageView in
-            priceIconImageView.centerY == priceIconImageView.superview!.centerY
-            
-            timeIconImageView.width == 60
-            timeIconImageView.height == 60
-            priceIconImageView.width == 60
-            priceIconImageView.height == 60
-            calendaIconImageView.width == 60
-            calendaIconImageView.height == 60
-            distribute(by: ICONS_MARGIN, horizontally: timeIconImageView, priceIconImageView, calendaIconImageView)
-            align(baseline: timeIconImageView, priceIconImageView, calendaIconImageView)
+        self.addSubview(timeIconImageView)
+        self.addSubview(priceIconImageView)
+        self.addSubview(calendaIconImageView)
+        
+        constrain(titleLabel,priceIconImageView){
+            titleLabel,priceIconImageView in
+            priceIconImageView.topMargin == titleLabel.bottomMargin + ICON_TITLE_MARGIN
         }
         
-        constrain(titleLabel,timeIconImageView){
-            titleLabel,timeIconImageView in
-            titleLabel.bottomMargin == timeIconImageView.topMargin + ICON_TITLE_MARGIN
+        layout(timeIconImageView,priceIconImageView,calendaIconImageView){
+            timeIconImageView,priceIconImageView,calendaIconImageView in
+            priceIconImageView.centerX == priceIconImageView.superview!.centerX
+            
+            timeIconImageView.width == ICON_SIZE
+            timeIconImageView.height == ICON_SIZE
+            priceIconImageView.width == ICON_SIZE
+            priceIconImageView.height == ICON_SIZE
+            calendaIconImageView.width == ICON_SIZE
+            calendaIconImageView.height == ICON_SIZE
+            distribute(by: ICONS_MARGIN, horizontally: timeIconImageView, priceIconImageView, calendaIconImageView)
+            align(top: timeIconImageView, priceIconImageView, calendaIconImageView)
         }
+        
     }
     
     func buildIconDesc(){
+        timeLabelView = UILabel(frame: CGRectZero)
+        priceLabelView = UILabel(frame: CGRectZero)
+        calendaLabelView = UILabel(frame: CGRectZero)
         
+        timeLabelView.textColor = UIColor.whiteColor()
+        priceLabelView.textColor = UIColor.whiteColor()
+        calendaLabelView.textColor = UIColor.whiteColor()
+        
+        timeLabelView.font = ICON_DESC_FONT
+        priceLabelView.font = ICON_DESC_FONT
+        calendaLabelView.font = ICON_DESC_FONT
+        
+        self.addSubview(timeLabelView)
+        self.addSubview(priceLabelView)
+        self.addSubview(calendaLabelView)
+        
+        
+        layout(timeLabelView,timeIconImageView){
+            timeLabelView,timeIconImageView in
+            timeLabelView.height == 21
+            timeLabelView.topMargin == timeIconImageView.bottomMargin + ICON_LABEL_MARGIN
+            timeLabelView.centerX == timeIconImageView.centerX
+        }
+        
+        layout(priceLabelView,priceIconImageView){
+            priceLabelView,priceIconImageView in
+            priceLabelView.height == 21
+            priceLabelView.topMargin == priceIconImageView.bottomMargin + ICON_LABEL_MARGIN
+            priceLabelView.centerX == priceIconImageView.centerX
+        }
+        
+        layout(calendaLabelView,calendaIconImageView){
+            calendaLabelView,calendaIconImageView in
+            calendaLabelView.height == 21
+            calendaLabelView.topMargin == calendaIconImageView.bottomMargin + ICON_LABEL_MARGIN
+            calendaLabelView.centerX == calendaIconImageView.centerX
+        }
+    }
+    
+    func fixFrame(){
+        self.frame.size.height = VIEW_HEIGHT
+    }
+}
+
+//上面图标下面文字标签的view
+class ServiceCardDetailIconLabelView : UIView{
+    var iconView : UIImageView!
+    var labelView : UILabel!
+    
+    func layoutView(){
+        iconView = UIImageView(frame: CGRectZero)
+        labelView = UILabel(frame: CGRectZero)
+        
+        layout(iconView,labelView){
+            iconView,labelView in
+            
+        }
     }
 }
