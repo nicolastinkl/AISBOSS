6681cb5ce771e4ae588d0ff5e07434607895e0d1
wangli <wangli13@aisainfo.com>
2015-11-23 17:40:15 +0800

    #add

---
+++ AISBOSS/Models/ocmodel/AIBuyerModels.m
@@ -0,0 +1,38 @@
+//
+//  AIBuyerModels.m
+//  AIVeris
+//
+//  Created by 王坜 on 15/11/23.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+#import "AIBuyerModels.h"
+
+
+
+
+
+@implementation AIBuyerModels
+
+@end
+
+#pragma mark - 详情评论部分
+@implementation AIMusicCommentsModel
+
+@end
+
+#pragma mark - 统计部分
+
+@implementation AIMusicChartModel
+
+
+@end
+
+
+
+#pragma mark - ServiceType
+
+@implementation AIMusicServiceTypesModel
+
+
+@end
--- AISBOSS/Views/SellerViews/AIStarRate.m
+++ AISBOSS/Views/SellerViews/AIStarRate.m
@@ -7,11 +7,15 @@
 //
 
 #import "AIStarRate.h"
+#import "AITools.h"
 
 @interface AIStarRate ()
 {
+    CGFloat _maxRate;
     CGFloat _starMargin;
     NSMutableArray *stars;
+    
+    UIView *_contentView;
 }
 
 
@@ -24,12 +28,87 @@
     self = [super initWithFrame:frame];
     
     if (self) {
-        
+        _displayRate = rate;
+        [self makeProperties];
+        [self makeStars];
     }
     
     return self;
 }
 
+
+#pragma mark - 设置Rate
+
+- (void)setDisplayRate:(CGFloat)displayRate
+{
+    if (displayRate > _maxRate) {
+        _displayRate = _maxRate;
+    }
+    else if (displayRate < 0)
+    {
+        _displayRate = 0;
+    }
+    else
+    {
+        _displayRate = displayRate;
+    }
+    
+    
+    [_contentView removeFromSuperview];
+    
+    [self makeProperties];
+    [self makeStars];
+}
+
+#pragma mark - 构造基本属性
+
+- (void)makeProperties
+{
+    _maxRate = 5.0;
+    _starMargin = [AITools displaySizeFrom1080DesignSize:8];
+    
+    _contentView = [[UIView alloc] initWithFrame:self.bounds];
+    [self addSubview:_contentView];
+    
+}
+
+#pragma mark - 构造Stars
+
+- (CGFloat)addStarImageWithName:(NSString *)name atX:(CGFloat)x
+{
+    CGFloat size = CGRectGetHeight(self.frame);
+    UIImage *star = [UIImage imageNamed:name];
+    UIImageView *starView = [[UIImageView alloc] initWithImage:star];
+    starView.frame = CGRectMake(x, 0, size, size);
+    [_contentView addSubview:starView];
+    
+    return x + size + _starMargin;
+}
+
+- (void)makeStars
+{
+    CGFloat lr = _displayRate ;
+    CGFloat x = _starMargin;
+    
+    while (lr - 1 >= 1) {
+        x = [self addStarImageWithName:@"Yellow_Star" atX:x];
+    }
+    
+    if (lr > 0) {
+        x = [self addStarImageWithName:@"Half_Star" atX:x];
+    }
+    
+    CGFloat or = _maxRate - _displayRate;
+    
+    while (or - 1 >= 1) {
+        x = [self addStarImageWithName:@"Gray_Star" atX:x];
+    }
+    
+    CGRect frame = self.frame;
+    frame.size.width = x - _starMargin;
+    self.frame = frame;
+    
+}
 
 
 
