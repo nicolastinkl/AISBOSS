b8a39968de7eafe250b08ae4bc111117b755bfe8
liuxian <liuxian@google.com>
2015-11-23 09:28:17 +0800

    #add 购物车view

---
+++ AISBOSS/ViewController/BuyerDetail/ServiceCardDetailShopping.swift
@@ -0,0 +1,24 @@
+//
+//  ServiceCardDetailShopping.swift
+//  AIVeris
+//
+//  Created by 刘先 on 15/11/20.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class ServiceCardDetailShopping: UIView {
+
+
+}
+
+class SCDShoppingListCellView : UIView {
+    
+    var imgView : UIImageView!
+    var descLabel : UILabel!
+    
+    func layoutView(){
+        
+    }
+}
