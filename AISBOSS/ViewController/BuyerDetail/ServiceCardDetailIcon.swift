c9a6f850fab7fa6b74c795450f1dc4ad0cd8a5e0
liuxian <liuxian@google.com>
2015-11-24 09:35:24 +0800

    #update 修改布局

--- AISBOSS/ViewController/BuyerDetail/ServiceCardDetailIcon.swift
+++ AISBOSS/ViewController/BuyerDetail/ServiceCardDetailIcon.swift
@@ -26,9 +26,9 @@ class ServiceCardDetailIcon: UIView {
     //MARK: - Constants
     //sizes
     let ICON_SIZE : CGFloat = AITools.displaySizeFrom1080DesignSize(62)
-    let VIEW_HEIGHT : CGFloat = AITools.displaySizeFrom1080DesignSize(300)
-    let VIEW_LEFT_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(40)
-    let VIEW_TOP_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(10)
+    let VIEW_HEIGHT : CGFloat = AITools.displaySizeFrom1080DesignSize(296)
+    let VIEW_LEFT_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(100)
+    let VIEW_TOP_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(48)
     let ICON_TITLE_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(68)
     let ICONS_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(200)
     let ICON_LABEL_MARGIN : CGFloat = AITools.displaySizeFrom1080DesignSize(64)
@@ -48,7 +48,7 @@ class ServiceCardDetailIcon: UIView {
     //MARK: - load data
     func loadData(){
         layoutView()
-        
+        self.backgroundColor = UIColor.greenColor()
         timeIconImageView.image = UIImage(named: "icon_time_big")
         priceIconImageView.image = UIImage(named: "icon_price_big")
         calendaIconImageView.image = UIImage(named: "icon_calenda_big")
@@ -77,9 +77,9 @@ class ServiceCardDetailIcon: UIView {
         self.addSubview(titleLabel)
         
         layout(titleLabel){titleLabel in
-            titleLabel.leadingMargin == titleLabel.superview!.leadingMargin
+            titleLabel.leadingMargin == titleLabel.superview!.leadingMargin + VIEW_LEFT_MARGIN
             titleLabel.trailingMargin >= titleLabel.superview!.trailingMargin
-            titleLabel.topMargin == titleLabel.superview!.topMargin
+            titleLabel.topMargin == titleLabel.superview!.topMargin + VIEW_TOP_MARGIN
             titleLabel.height == 20
         }
     }
