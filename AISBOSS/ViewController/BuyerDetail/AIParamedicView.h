7d42f86a1bafbb57c2b295488e5a1930ef807710
wangli <wangli13@aisainfo.com>
2015-11-24 11:10:21 +0800

    #add

---
+++ AISBOSS/ViewController/BuyerDetail/AIParamedicView.h
@@ -0,0 +1,13 @@
+//
+//  AIParamedicView.h
+//  AIVeris
+//
+//  Created by 王坜 on 15/11/24.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIParamedicView : UIView
+
+@end
