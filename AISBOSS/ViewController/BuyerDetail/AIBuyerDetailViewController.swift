8ededdf895f89bcfb6a3bec09f4fa1246b60f26b
nicolastinkl <nicolastinkl@gmail.com>
2015-12-02 17:52:54 +0800

    # update bubleModel

--- AISBOSS/ViewController/BuyerDetail/AIBuyerDetailViewController.swift
+++ AISBOSS/ViewController/BuyerDetail/AIBuyerDetailViewController.swift
@@ -130,7 +130,7 @@ class AIBuyerDetailViewController : UIViewController {
     
     func InitController(){
         
-        let name = dataSource?.proposal_name ?? ""
+        let name = bubleModel?.proposal_name ?? ""
         self.backButton.setTitle(name, forState: UIControlState.Normal)
         self.moneyLabel.text = dataSource?.order_total_price
         
