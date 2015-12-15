3b0982052169741573e54811f11024b38e1c7006
wangli <wangli13@aisainfo.com>
2015-12-15 11:48:10 +0800

    优化登录动画

---
+++ AISBOSS/Utils/AINotifications.h
@@ -0,0 +1,12 @@
+//
+//  AINotifications.h
+//  AIVeris
+//
+//  Created by 王坜 on 15/12/15.
+//  Copyright © 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+
+#define kShouldUpdataUserDataNotification     @"ShouldUpdataUserDataNotification"
+
+
