2f1e1a4e88446aef1404ec27e4eed77db5ec6d5e
liliang <liliang9@asiainfo.com>
2015-12-22 09:47:39 +0800

    修改

--- AISBOSS/ViewController/ExpandContentViewFactory.swift
+++ AISBOSS/ViewController/ExpandContentViewFactory.swift
@@ -10,18 +10,20 @@ import UIKit
 
 
 class ServiceOrderExpandContentViewFactory {
-    static func createExpandContentView(param: InfoDetailModel) -> UIView? {
+    static func createExpandContentView(param: ParamModel) -> UIView? {
         var contentView: UIView?
-        if param.type == 25043309 {
-            let expandContent = ImageContent(frame: CGRect(x: 0, y: 0, width: 0, height: 180))
-            
-            expandContent.imgUrl = param.content
-            
-            contentView = expandContent
-        } else if param.type == 25043310 {
+        if param.param_key == "25043309" {
+            if param.param_value != "" {
+                let expandContent = ImageContent(frame: CGRect(x: 0, y: 0, width: 0, height: 180))
+                
+                expandContent.imgUrl = param.param_value
+                
+                contentView = expandContent
+            }          
+        } else if param.param_key == "25043310" {
             let expandContent = AIOrderCellEShopView(frame: CGRect(x: 0, y: 0, width: 0, height: 200))
             
-            let s: NSString = param.content
+            let s: NSString = param.param_value
             let convertString = s.stringByReplacingOccurrencesOfString("\\", withString: "")
             print(convertString)
             let itemList = GoodsListMode(string: convertString, error: nil)
