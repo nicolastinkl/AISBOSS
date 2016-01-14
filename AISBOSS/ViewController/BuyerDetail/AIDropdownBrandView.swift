4137835d60415a34ee4e894c90e27e2a10c9a82e
zhengxin <zx19880528@gmail.com>
2016-01-14 17:29:14 +0800

    完成 可滚动的 brandView

--- AISBOSS/ViewController/BuyerDetail/AIDropdownBrandView.swift
+++ AISBOSS/ViewController/BuyerDetail/AIDropdownBrandView.swift
@@ -14,16 +14,26 @@ class AIDropdownBrandView: UIView {
 			// TODO: animation
 		}
 	}
+    
+    var expandedView: UIView!
 	var brands = [String]()
 	var labels = [HalfRoundedCornerLabel]()
 	var downButton: UIButton!
 	var barView: UIView! // 上面条状的 bar 背景
+	var lineView: UIView!
+	var selectedIndex: Int {
+		didSet {
+			updateLabelSelectStatus()
+		}
+	}
 	
-	private struct Constants {
-		static let barHeight: CGFloat = 20
-		static let downButtonWidth: CGFloat = 20
+	struct Constants {
+		static let barHeight: CGFloat = AITools.displaySizeFrom1080DesignSize(88)
+		static let downButtonWidth: CGFloat = 44
 		static let margin: CGFloat = 8
-		
+		static let space: CGFloat = 20
+		static let lineViewHeight: CGFloat = 0.5
+		static let barViewBackgroundColor: UIColor = UIColor(red: 0.1176, green: 0.1059, blue: 0.2196, alpha: 1.0)
 		struct Label {
 			static let backgroundColor: UIColor = UIColor.clearColor()
 			static let highlightedBackgroundColor: UIColor = UIColor(red: 0.2941, green: 0.2863, blue: 0.3765, alpha: 1.0)
@@ -34,20 +44,33 @@ class AIDropdownBrandView: UIView {
 	
 	init(brands: [String], selectedIndex: Int, width: CGFloat) {
 		self.brands = brands
+		self.selectedIndex = selectedIndex
 		super.init(frame: CGRectMake(0, 0, width, Constants.barHeight))
 		setupBarView()
 		setupDownButton()
+        setupExpandedView()
 		setupLabels()
-		
+		setupLineView()
+		updateLabelSelectStatus()
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+    
+    func setupExpandedView() {
+        expandedView = UIView(frame: .zero)
+        addSubview(expandedView)
+        setupIconLabels()
+    }
+    
+    func setupIconLabels() {
+        
+    }
 	
 	func setupBarView() {
 		barView = UIView(frame: .zero)
-		barView.backgroundColor = UIColor(red: 0.1176, green: 0.1059, blue: 0.2196, alpha: 1.0)
+		barView.backgroundColor = Constants.barViewBackgroundColor
 		addSubview(barView)
 		barView.snp_makeConstraints {(make) -> Void in
 			make.top.leading.trailing.equalTo(self)
@@ -56,36 +79,69 @@ class AIDropdownBrandView: UIView {
 	}
 	
 	func setupLabels() {
-		for brand in brands {
-			
+		let barScrollView = UIScrollView(frame: .zero)
+		barView.addSubview(barScrollView)
+		for (i, brand) in brands.enumerate() {
 			let label = HalfRoundedCornerLabel(text: brand, backgroundColor: Constants.Label.backgroundColor, highlightedBackgroundColor: Constants.Label.highlightedBackgroundColor, textColor: Constants.Label.textColor, highlightedTextColor: Constants.Label.highlightedTextColor)
 			label.text = brand
-			addSubview(label)
+			label.tag = i
+			label.userInteractionEnabled = true
+			barScrollView.addSubview(label)
 			labels.append(label)
-            let tap = UITapGestureRecognizer(target: self, action: "tapped:")
-            label.addGestureRecognizer(tap)
+			let tap = UITapGestureRecognizer(target: self, action: "tapped:")
+			label.addGestureRecognizer(tap)
 		}
-		let stackView = OAStackView(arrangedSubviews: labels)
-		stackView.distribution = .EqualSpacing
-		stackView.axis = .Horizontal
-		addSubview(stackView)
 		
-		stackView.snp_makeConstraints {(make) -> Void in
+		barScrollView.snp_makeConstraints {(make) -> Void in
+			make.top.equalTo(barView)
 			make.leading.equalTo(barView).offset(Constants.margin)
-			make.top.bottom.equalTo(barView)
+			make.bottom.equalTo(barView).offset(-Constants.lineViewHeight)
 			make.trailing.equalTo(downButton.snp_leading).offset(-Constants.margin)
 		}
+		
+		var previousView: UIView = barScrollView
+		for (i, label) in labels.enumerate() {
+			label.snp_makeConstraints(closure: {(make) -> Void in
+					if i == 0 {
+						// first label
+						make.leading.equalTo(previousView)
+					} else if i == labels.count - 1 {
+						// last label
+						make.leading.equalTo(previousView.snp_trailing).offset(Constants.space)
+						make.trailing.equalTo(barScrollView)
+					} else {
+						// center labels
+						make.leading.equalTo(previousView.snp_trailing).offset(Constants.space)
+					}
+					// every label
+					make.bottom.equalTo(barScrollView)
+					make.top.equalTo(barScrollView).offset(10)
+				})
+			previousView = label
+		}
 	}
-    
-    func tapped(g: UITapGestureRecognizer) {
-        labels.forEach { (label) -> () in
-            if label == g.view {
-                label.highlighted = true
-            } else {
-                label.highlighted = false
-            }
-        }
-    }
+	
+	func setupLineView() {
+		let line = UIView(frame: .zero)
+		line.backgroundColor = Constants.Label.highlightedBackgroundColor
+		barView.addSubview(line)
+		line.snp_makeConstraints {(make) -> Void in
+			make.leading.equalTo(barView).offset(Constants.margin / 2)
+			make.trailing.bottom.equalTo(barView)
+			make.height.equalTo(Constants.lineViewHeight)
+		}
+	}
+	
+	func updateLabelSelectStatus() {
+		for (i, label) in labels.enumerate() {
+			label.highlighted = (i == selectedIndex)
+		}
+	}
+	
+	func tapped(g: UITapGestureRecognizer) {
+		selectedIndex = g.view!.tag
+		updateLabelSelectStatus()
+	}
 	
 	func setupDownButton() {
 		downButton = UIButton(type: .Custom)
@@ -107,33 +163,52 @@ class AIDropdownBrandView: UIView {
 
 class HalfRoundedCornerLabel: UILabel {
 	var highlightedBackgroundColor: UIColor
+	var normalBackgroundColor: UIColor
+	
+	struct Constants {
+		static let cornerRadii = CGSize(width: AITools.displaySizeFrom1080DesignSize(10), height: AITools.displaySizeFrom1080DesignSize(10))
+		static let margin = AITools.displaySizeFrom1080DesignSize(24)
+		static let font = AITools.myriadLightSemiCondensedWithSize(AITools.displaySizeFrom1080DesignSize(41))
+	}
+	
 	override var highlighted: Bool {
 		didSet {
-			self.backgroundColor = self.highlighted ? self.highlightedBackgroundColor : self.backgroundColor
+			backgroundColor = highlighted ? highlightedBackgroundColor : normalBackgroundColor
+		}
+	}
+	
+	override var frame: CGRect {
+		didSet {
+			setupCorners()
 		}
 	}
 	
 	override func intrinsicContentSize() -> CGSize {
-		return CGSizeMake(super.intrinsicContentSize().width + 16, super.intrinsicContentSize().height)
+		return CGSizeMake(super.intrinsicContentSize().width + Constants.margin * 2, super.intrinsicContentSize().height + 10)
+		// 10 is magic number hehe.
 	}
+	
 	
 	init(text: String, backgroundColor: UIColor, highlightedBackgroundColor: UIColor, textColor: UIColor, highlightedTextColor: UIColor) {
 		self.highlightedBackgroundColor = highlightedBackgroundColor
+		self.normalBackgroundColor = backgroundColor
 		super.init(frame: CGRect.zero)
 		self.text = text
+		self.font = Constants.font
 		self.textColor = textColor
 		self.highlightedTextColor = highlightedTextColor
 		self.textAlignment = .Center
 		self.backgroundColor = backgroundColor
-//		self.setupCorners()
+		self.frame = CGRectMake(0, 0, intrinsicContentSize().width, intrinsicContentSize().height)
+		self.setupCorners()
 	}
 	
 	func setupCorners() {
-		let maskPath = UIBezierPath(roundedRect: bounds, byRoundingCorners: [.TopLeft, .TopRight], cornerRadii: CGSizeMake(5, 5))
+		let maskPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: [.TopLeft, .TopRight], cornerRadii: Constants.cornerRadii)
 		let maskLayer = CAShapeLayer()
-		maskLayer.frame = bounds
+		maskLayer.frame = self.bounds
 		maskLayer.path = maskPath.CGPath
-		layer.mask = maskLayer
+		self.layer.mask = maskLayer
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
--- AISBOSS/Views/VerticalIconLabel.swift
+++ AISBOSS/Views/VerticalIconLabel.swift
@@ -104,12 +104,8 @@ class VerticalIconLabel: UIView {
 			layoutIfNeeded()
 		}
 	}
-	
-	
+    
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
-	
-	
 }
