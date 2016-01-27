95d55fe1193ca833d425941503104eb5403e3eab
wangli <wangli13@aisainfo.com>
2016-01-27 11:06:48 +0800

    去掉冗余数据

--- AISBOSS/Models/ProposalModel.h
+++ AISBOSS/Models/ProposalModel.h
@@ -431,13 +431,15 @@ typedef NS_ENUM(NSInteger, ParamSettingFlag) {
 @property (nonatomic, strong) AIProposalServicePriceModel<Optional> * service_price;
 @property (nonatomic, strong) AIProposalServiceDetailProviderModel<Optional> * service_provider; // ok
 
-@property (nonatomic, strong) NSArray<Optional> * service_param_list;
-@property (nonatomic, strong) NSArray<Optional> * service_param_rel_list;
+
 
 @property (nonatomic, strong) AIProposalServiceDetailRatingModel<Optional> * service_rating; // ok
 @property (nonatomic, strong) AIProposalServiceDetail_WishModel<Optional> * wish_list;       // ok
 @property (nonatomic, strong) NSArray<AIProposalServiceDetailRatingCommentModel, Optional> *comment_list;
 
+// new
+@property (nonatomic, strong) NSArray<Optional> * service_param_list;
+@property (nonatomic, strong) NSArray<Optional> * service_param_rel_list;
 @property (nonatomic, strong) NSArray<Optional> *service_param_display_list;
 
 @end
