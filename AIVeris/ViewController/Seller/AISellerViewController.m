d941390a6f3a971cf6231fef5d502df72b788694
wangli <wangli13@aisainfo.com>
2016-03-18 09:40:58 +0800

    007 版本

--- AIVeris/ViewController/Seller/AISellerViewController.m
+++ AIVeris/ViewController/Seller/AISellerViewController.m
@@ -484,7 +484,7 @@
     [view addSubview:timeLabel];
     
     
-    return view;
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -570,7 +570,7 @@
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return 20;
+    return 0;
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
 {
