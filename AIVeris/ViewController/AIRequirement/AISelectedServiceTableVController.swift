188da1cd54bc870b598713d46df594e6afb5bd17
nicolastinkl <nicolastinkl@gmail.com>
2016-03-23 11:09:53 +0800

    # 提交更新版本，从Cell展开的情况改变成弹出式的UIViewController展示

---
+++ AIVeris/ViewController/AIRequirement/AISelectedServiceTableVController.swift
@@ -0,0 +1,19 @@
+//
+//  UISelectedServiceTableVController.swift
+//  AIVeris
+//
+//  Created by tinkl on 3/23/16.
+//  Copyright © 2016 ___ASIAINFO___. All rights reserved.
+//
+
+import Foundation
+
+class AISelectedServiceTableVController: UIViewController {
+    
+    func viewDidLoad(){
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
