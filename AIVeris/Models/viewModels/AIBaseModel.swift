2bebf080e7a62df83c6daf88d330fc86acdcef7e
liuxian <liuxian@google.com>
2016-03-23 17:19:04 +0800

    #update 权限设置改为弹出框

--- AIVeris/Models/viewModels/AIBaseModel.swift
+++ AIVeris/Models/viewModels/AIBaseModel.swift
@@ -9,5 +9,35 @@
 import Foundation
 
 class AIBaseViewModel : NSObject {
+ 
+    class func printModelContent(obj : AnyObject) -> String{
+        var outString = ""
+        var outCount : UInt32 = 0
+        let propertyList : UnsafeMutablePointer<objc_property_t>! = class_copyPropertyList(obj.classForCoder, &outCount)
+        let count = Int(outCount)
+        for i in 0...(count - 1){
+            let aPro: objc_property_t = propertyList[i]
+            let proName:String! = String(UTF8String: property_getName(aPro))
+            outString += "\(proName) : \(String(obj.valueForKey(proName)!)) \r\n"
+        }
+        return outString
+    }
     
+    class func printArrayModelContent(array : Array<AnyObject>) -> String{
+        var outString = ""
+        for (index,item) in array.enumerate(){
+            outString += "\(index) :"
+            
+            var outCount : UInt32 = 0
+            let propertyList : UnsafeMutablePointer<objc_property_t>! = class_copyPropertyList(item.classForCoder, &outCount)
+            let count = Int(outCount)
+            for i in 0...(count - 1){
+                let aPro: objc_property_t = propertyList[i]
+                let proName:String! = String(UTF8String: property_getName(aPro))
+                outString += "\(proName) : \(String(item.valueForKey(proName)!)) \r\n"
+            }
+            outString += "\r\n"
+        }
+        return outString
+    }
 }
