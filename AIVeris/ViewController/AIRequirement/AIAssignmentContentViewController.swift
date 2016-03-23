a19c2fb66383241639495676456ae0262e7067ac
nicolastinkl <nicolastinkl@gmail.com>
2016-03-23 15:05:53 +0800

    # 处理发光效果

--- AIVeris/ViewController/AIRequirement/AIAssignmentContentViewController.swift
+++ AIVeris/ViewController/AIRequirement/AIAssignmentContentViewController.swift
@@ -15,6 +15,7 @@ class AIAssignmentContentViewController: UIViewController {
     
     
     @IBOutlet weak var contentLabel: UILabel!
+        
     
     @IBOutlet weak var tarButton: DesignableButton!
     
@@ -24,6 +25,16 @@ class AIAssignmentContentViewController: UIViewController {
         contentLabel.font = AITools.myriadLightSemiCondensedWithSize(AITools.displaySizeFrom1080DesignSize(52))
         
         tarButton.titleLabel?.font = AITools.myriadSemiCondensedWithSize(AITools.displaySizeFrom1080DesignSize(52))
+        
+        /*MDCSpotlightView *focalPointView = [[MDCSpotlightView alloc] initWithFocalView:self];
+        focalPointView.bgColor= [UIColor whiteColor];
+        focalPointView.frame = CGRectMake(0, 0, size + 13, size + 13);
+        focalPointView.center = CGPointMake(self.width/2, self.height/2);
+        focalPointView.layer.cornerRadius = focalPointView.frame.size.width/2;
+        focalPointView.layer.masksToBounds  = YES;
+        [focalPointView setNeedsDisplay];
+        [self insertSubview:focalPointView atIndex:0];
+        focalPointView.alpha = kDefaultAlpha;*/
         
     }
     
@@ -38,6 +49,37 @@ class AIAssignmentContentViewController: UIViewController {
         }
         
     }
+    
+    
+    
+    /*
+    
+    
+    - (void)TimerEvent
+    {
+    MDCSpotlightView *focalPointView = self.timer.userInfo[@"focalPointView"];
+    
+    if (focalPointView != nil) {
+    CGFloat alpha = focalPointView.alpha;
+    if ( alpha == 0.5) {
+    [UIView animateWithDuration:0.8 animations:^{
+    focalPointView.alpha = 0.0f;
+    } completion:^(BOOL finished) {
+    
+    }];
+    }else{
+    [UIView animateWithDuration:0.8 animations:^{
+    focalPointView.alpha = 0.5;
+    } completion:^(BOOL finished) {
+    
+    }];
+    }
+    
+    }
+    
+    }
+
+    */
     
 }
 
