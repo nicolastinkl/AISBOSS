e9c4d07dbafb000b16d51697673e8810ffc09022
nicolastinkl <nicolastinkl@gmail.com>
2016-03-24 17:06:18 +0800

    # 兼容ios8 处理crash

--- AIVeris/ViewController/AIRequirement/BuyerRequirmentMessageViewController.swift
+++ AIVeris/ViewController/AIRequirement/BuyerRequirmentMessageViewController.swift
@@ -12,6 +12,13 @@ class BuyerRequirmentMessageViewController: UIViewController {
     @IBOutlet weak var topView: UIView!
     @IBOutlet weak var tableView: UITableView!
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
