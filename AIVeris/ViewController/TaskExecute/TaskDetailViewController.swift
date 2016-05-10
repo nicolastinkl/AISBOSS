0c3a0020f2c67e603bba12961e7907e822c51103
liliang <liliang9@asiainfo.com>
2016-05-10 10:13:11 +0800

    添加自定义xib控件的初始化方法

---
+++ AIVeris/ViewController/TaskExecute/TaskDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TaskDetailViewController.swift
+//  AIVeris
+//
+//  Created by admin on 16/5/9.
+//  Copyright © 2016年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    @IBOutlet weak var serviceTime: IconLabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        serviceTime.label.text = "dfasfasdfafad"
+        serviceTime.icon.image = UIImage(named: "airplane")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
