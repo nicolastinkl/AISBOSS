85c33ffe011c0987b5db555df85fb895e0f8d6ee
liliang <liliang9@asiainfo.com>
2016-05-10 17:47:02 +0800

    创建了提交任务初始的界面

---
+++ AIVeris/ViewController/TaskExecute/TaskResultCommitViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TaskResultCommitViewController.swift
+//  AIVeris
+//
+//  Created by Rocky on 16/5/10.
+//  Copyright © 2016年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class TaskResultCommitViewController: UIViewController {
+
+    @IBOutlet weak var checkIcon: UIImageView!
+    @IBOutlet weak var writeIcon: UIImageView!
+    @IBOutlet weak var cameraIcon: UIImageView!
+    @IBOutlet weak var questButton: UIButton!
+    
+    @IBOutlet weak var line1: UIView!
+    @IBOutlet weak var line2: UIView!
+    @IBOutlet weak var line3: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        questButton.layer.cornerRadius = questButton.height / 2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
