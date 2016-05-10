0351801121bd78e264d3497d525a2e10ab601295
liliang <liliang9@asiainfo.com>
2016-05-10 14:30:32 +0800

    完成服务细节粗略布局

--- AIVeris/ViewController/TaskExecute/TaskDetailViewController.swift
+++ AIVeris/ViewController/TaskExecute/TaskDetailViewController.swift
@@ -11,12 +11,21 @@ import UIKit
 class TaskDetailViewController: UIViewController {
 
     @IBOutlet weak var serviceTime: IconLabel!
+    @IBOutlet weak var serviceLocation: IconLabel!
+    @IBOutlet weak var QRCodeImage: ServiceQRCodeView!
+    @IBOutlet weak var promptCheckIn: UILabel!
+    @IBOutlet weak var bottomButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bottomButton.layer.cornerRadius = bottomButton.height / 2
 
         serviceTime.label.text = "dfasfasdfafad"
         serviceTime.icon.image = UIImage(named: "airplane")
-        // Do any additional setup after loading the view.
+        
+        serviceLocation.label.text = "ga bvqujencuqewncwech vqeirchnqwcc evcqcqewcewccewc qveqcwecq3exqtbhynymu"
+        serviceLocation.icon.image = UIImage(named: "ai_custom_diy")
     }
 
     override func didReceiveMemoryWarning() {
