554ccd6991ae93bd7691862c1689ab1f14cb5959
wangli <wangli13@aisainfo.com>
2016-05-11 10:07:52 +0800

    add anchor

---
+++ AIVeris/ViewController/AudioAssistant/Models/AIAnchor.swift
@@ -0,0 +1,48 @@
+//
+//  AIAnchor.swift
+//  AIVeris
+//
+//  Created by 王坜 on 16/5/10.
+//  Copyright © 2016年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+enum AIAnchorType {
+    case Before              // 事前
+    case executing           // 事中
+    case After               // 事后
+}
+
+
+
+
+
+
+class AIAnchor: NSObject {
+    
+    var type : AIAnchorType?         // 锚点类型
+    var selector : String?           // 方法名
+    var parameters : [AnyObject]?    // 参数列表
+    
+    
+    
+    
+    
+    
+    class func handleAnchorWithType(type:AIAnchorType, selector:String, parameters:[AnyObject]) -> String {
+        
+        
+        
+        
+        
+        return ""
+    }
+    
+    
+    
+    
+    
+    
+    
+}
