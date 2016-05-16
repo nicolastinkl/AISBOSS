8a3c7c4f9c23af936e647748fb771547dc1e3700
zhengxin <zx19880528@gmail.com>
2016-05-16 11:35:00 +0800

    tmp

--- AIVeris/ViewController/AudioAssistant/Models/AIAnchor.swift
+++ AIVeris/ViewController/AudioAssistant/Models/AIAnchor.swift
@@ -8,20 +8,23 @@
 
 import UIKit
 
-typealias AIAnchorStep = String
-typealias AIAnchorType = String
 
-//enum AIAnchorStep : String {
-//    case Before = "Before"             // 事前
-//    case Executing = "Executing"       // 事中
-//    case After = "After"               // 事后
-//    case Hold = "Hold"                 // 独占
-//    case UnHold = "UnHold"             // 解除独占
-//    static let allValues = [Before, Executing, After, Hold]
-//}
 
-extension AIAnchorStep {
-    static let Normal = "Normal"
+enum AIAnchorStep : String {
+    case Before = "Before"             // 事前
+    case Executing = "Executing"       // 事中
+    case After = "After"               // 事后
+    case Hold = "Hold"                 // 独占
+    case UnHold = "UnHold"             // 解除独占
+    static let allValues = [Before, Executing, After, Hold]
+}
+
+
+enum AIAnchorType : String {
+    case Touch = "Touch"             // Touch事件
+    case Normal = "Normal"           // 普通Anchor事件
+
+    static let allValues = [Touch, Normal]
 }
 
 class CustomWindow: UIWindow {
@@ -30,14 +33,6 @@ class CustomWindow: UIWindow {
         return view
     }
 }
-//
-//enum AIAnchorType : String {
-//    case Touch = "Touch"             // Touch事件
-//    case Normal = "Normal"           // 普通Anchor事件
-//
-//    static let allValues = [Touch, Normal]
-//}
-
 
 struct AIAnchorKeys {
     static let Type = "Type"
@@ -47,8 +42,7 @@ struct AIAnchorKeys {
 
 
 
-
-struct AIAnchor: JSONJoy {
+class AIAnchor: NSObject {
     
     var type : AIAnchorType?         // 锚点类型
     var step : AIAnchorStep?        // 锚点步骤
@@ -57,18 +51,11 @@ struct AIAnchor: JSONJoy {
     var parameters : [AnyObject]?    // 参数列表
     
     
-    init(_ decoder: JSONDecoder)  {
-        type = decoder["type"].string
-        step = decoder["step"].string
-        className = decoder["className"].string
-        selector = decoder["selector"].string
-        parameters =  decoder["parameters"].array
-    }
-    
-    init() {}
     
     
-    static func handleAnchorWithType(type:AIAnchorType, selector:String, parameters:[AnyObject]) -> String {
+    
+    
+    class func handleAnchorWithType(type:AIAnchorType, selector:String, parameters:[AnyObject]) -> String {
         
         
         
